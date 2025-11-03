31d8a950c39fc98120b3f1c7bd491f09f1b37a52
Anthony2080 <antonio.pirotta@davinci.edu.ar>
2025-11-03 15:37:09 -0300

    Inicio Contact
    se deja el inicio del contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import {Routes , Route} from 'react-router-dom'
-import NavBar from './components/NavBar'
+import NavBar from './components/Navbar'
 import Home from './pages/Home'
+import Contact from './pages/Contact'
 import About from './pages/About'
 import Footer from './components/Footer' 
 import Silksong from './pages/Silksong'
 import CoreKeeper from './pages/CoreKeeper'
 import Peak from './pages/Peak'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +20,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path='/silksong' element={<Silksong />} />
         <Route path='/corekeeper' element={<CoreKeeper />} />
         <Route path='/peak' element={<Peak />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,7 @@ export default function Navbar() {
             <ul id="menuList" className={menuOpen ? "active" : ""}>
                 <li><Link to={"/"}>Inicio</Link></li>
                 <li><Link to={"/about"}>Acerca de </Link></li>
+                <li><Link to={"/contact"}>Contacto </Link></li>
             </ul>
             <div>
                 <div className="menu-icon" onClick={toggleMenu}>
